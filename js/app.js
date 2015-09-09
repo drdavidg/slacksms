80eff8c08ef8d480408de3d249c298f0631d790c
David Gonen <david@curiousminds.com>
2015-09-09 19:44:07 +0300

    added phone number field, and new channel is created for each unique phone number.

--- js/app.js
+++ js/app.js
@@ -4,49 +4,49 @@ $($(document).ready( function() {
 
 	openSocket();
 
-function openSocket() {
+	function openSocket() {
 
-	$.getJSON('https://slack.com/api/rtm.start',
-	 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652'
-		},
-		function(json, textStatus) {
-			//console.log(json.channels[2].latest.text);
-			//addReceivedMsgtoDOM(json.channels[2].latest.text);
-			var connection = new WebSocket(json.url);
-			connection.onopen = function () {
-				console.log("ws url is  : " + json.url);
-				console.log("web socket connected!!! (i think)");
-				// var msg = {
-				// 	type: "message",
-				// 	id: 1,
-				// 	channel: "C0ABS4QNL",
-				// 	text: "sending a messssaaaage"
-				// };
-				// connection.send(JSON.stringify(msg));
-			};
-			connection.onerror = function (error) {
-		  	console.log('Error Logged: ' + error); //log errors
-			};
-			connection.onmessage = function (e) {
-				console.log(e.data);
-				var parsedData = JSON.parse(e.data);
-				console.log("message text:  " + parsedData.text);
-				if ((parsedData.text !== undefined)&&(parsedData.subtype !== "bot_message")) addReceivedMsgtoDOM(parsedData.text);
-			};
+		$.getJSON('https://slack.com/api/rtm.start',
+		 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652'
+			},
+			function(json, textStatus) {
+				//console.log(json.channels[2].latest.text);
+				//addReceivedMsgtoDOM(json.channels[2].latest.text);
+				var connection = new WebSocket(json.url);
+				connection.onopen = function () {
+					// console.log("ws url is  : " + json.url);
+					// console.log("web socket connected!!! (i think)");
+					// var msg = {
+					// 	type: "message",
+					// 	id: 1,
+					// 	channel: "C0ABS4QNL",
+					// 	text: "sending a messssaaaage"
+					// };
+					// connection.send(JSON.stringify(msg));
+				};
+				connection.onerror = function (error) {
+			  	console.log('Error Logged: ' + error); //log errors
+				};
+				connection.onmessage = function (e) {
+				 console.log(e.data);
+					var parsedData = JSON.parse(e.data);
+				//	console.log("message text:  " + parsedData.text);
+					//console.log("reply to:  " + parsedData.reply_to);
+					if ((parsedData.text !== undefined)&&(parsedData.subtype !== "bot_message")&&(parsedData.reply_to === undefined)&&(parsedData.subtype !== "channel_join")) addReceivedMsgtoDOM(parsedData.text);
+				};
 
-		});
+			});
 
-
-}
-
-
+	}
 
 	$('div.container').on('click', 'form.msgform button.sendmsgbutton', function(event) {
 		event.preventDefault();
 		/* Act on the event */
 		console.log('button clicked');
 		var msgContent = $('input.msginput').val();
-		addMsgtoSlack(msgContent);
+		var phoneNumber = $('input.phoneinput').val();
+		console.log(phoneNumber);
+		setChannel(msgContent, phoneNumber);
 		addSentMsgtoDOM(msgContent);
 	});
 
@@ -63,17 +63,50 @@ function openSocket() {
 		$(html).appendTo('.chatarea');
 	}
 
-	function addMsgtoSlack(msgContent) {
-			$.getJSON('https://slack.com/api/chat.postMessage',
-			 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
-				channel: "#working-channel",
-				text: msgContent,
-				username: "Guest3201"
-				},
-				function(json, textStatus) {
-						/*optional stuff to do after success */
-						console.log("message was postered!");
-				});
+	function setChannel(msgContent, channelName) {
+		// if channelName doesn't exists on error make the channelname first
+		//i thihnk have to switch to $.ajax.  does $
+		$.ajax({
+			url: 'https://slack.com/api/channels.create',
+			type: 'GET',
+			dataType: 'json',
+			data: {
+				token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
+				name: channelName,
+				scope: "admin"}
+		})
+		.done(function(json) {
+			console.log("success");
+			console.log("channelName is: " + channelName + " msgContent is " + msgContent);
+			console.log("stringified channelName " + JSON.stringify(channelName));
+			console.log(json.channel.id);
+
+
+			addMsgtoSlack(msgContent, json.channel.id);
+		})
+		.fail(function() {
+			console.log("fail");
+			return false;
+		})
+		.error(function() {
+			/* Act on the event */
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	}
+
+	function addMsgtoSlack(msgContent, channelName) {
+		$.getJSON('https://slack.com/api/chat.postMessage', {
+			token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
+			channel: channelName,
+			text: msgContent
+		},
+		function(json, textStatus) {
+				/*optional stuff to do after success */
+				console.log('chat should be posted to channel????  ' + channelName);
+		});
 	}
 
 	// var oldLatestMsg = "";
