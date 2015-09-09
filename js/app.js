b92de8069982b08d5782dce21bfa13f581001e00
David Gonen <david@curiousminds.com>
2015-09-09 12:15:37 +0300

    sending messages from app-> slack works, and updates the DOM.  going to look into getting messages slack->app, have to use slacks realtime API

--- js/app.js
+++ js/app.js
@@ -1,37 +1,60 @@
 $($(document).ready( function() {
 
-	$.getJSON('https://slack.com/api/channels.create',
-	 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
-		name: "working channel",
-		scope: "admin"
-		}, function(json, textStatus) {
-			/*optional stuff to do after success */
-			console.log("gots the jsons");
-	});
+		//commenting out because i already created this channel (using api)
+		// $.getJSON('https://slack.com/api/channels.create',
+		//  {token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
+		// 	name: "working channel",
+		// 	scope: "admin"
+		// 	}, function(json, textStatus) {
+		// 		/*optional stuff to do after success */
+		// 		console.log("gots the jsons");
+		// });
 
-	$('.msgform').submit( function(event){
-		console.log("form submitted!!");
+	// $('form.msgform').submit( function(event){
+	// 	console.log("form submitted!!");
+	//
+	// 	var msgContent = $('input.msginput').val();
+	// 	console.log(msgContent);
+	// 	addMsgtoSlack(msgContent);
+	// 	addSentMsgtoDOM(msgContent);
+	// 	return false;
+	// });
 
+	$('div.container').on('click', 'form.msgform button.sendmsgbutton', function(event) {
+		event.preventDefault();
+		/* Act on the event */
+		console.log('button clicked');
 		var msgContent = $('input.msginput').val();
-		console.log(msgContent);
-		addMsg(msgContent);
-
-		return false;
-	});
-
-function addMsg(msgContent) {
-
-	$.getJSON('https://slack.com/api/chat.postMessage',
-	 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
-		channel: "#working-channel",
-		text: msgContent
-		}, function(json, textStatus) {
-				/*optional stuff to do after success */
-				console.log("message was postered!");
+		addMsgtoSlack(msgContent);
+		addSentMsgtoDOM(msgContent);
 	});
 
 
-}
+	function addSentMsgtoDOM(msgContent) {
+		var html = "";
+		html += "<div class='sentmsg msg pure-g'><div class=pure-u-1-8'></div><div class='pure-u-3-4'><div class='bubbleright'><div class='msgtext'>" + msgContent + "</div></div></div><div class='pure-u-1-8'></div></div>";
+		$(html).appendTo('.chatarea');
+	}
+
+	function addReceivedMsgtoDOM(msgContent) {
+		var html = "";
+		html += "<div class='receivedmsg msg pure-g'><div class=pure-u-1-8'></div><div class='pure-u-3-4'><div class='bubbleleft'><div class='msgtext'>" + msgContent + "</div></div></div><div class='pure-u-1-8'></div></div>";
+		$(html).appendTo('.chatarea');
+	}
+
+	function addMsgtoSlack(msgContent) {
+
+			$.getJSON('https://slack.com/api/chat.postMessage',
+			 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
+				channel: "#working-channel",
+				text: msgContent,
+				username: "Guest3201"
+				}, function(json, textStatus) {
+						/*optional stuff to do after success */
+						console.log("message was postered!");
+			});
+
+	}
 
 
 }));
