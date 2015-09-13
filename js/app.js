cc70f50d323018b4afbd5ccd0a732837dbe43a23
David Gonen <david@curiousminds.com>
2015-09-13 22:08:17 +0300

    everything working, using websockets.  can't get both the bot user and the slack user to have their profile pictures show up always. TODO: need to check for new messages in more than just the 1 latest message.

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ $($(document).ready( function() {
 					 //console.log(e.data);
 						var parsedData = JSON.parse(e.data);
 						if ((parsedData.text !== undefined)&&(parsedData.subtype !== "bot_message")&&(parsedData.reply_to === undefined)&&(parsedData.subtype !== "channel_join")) {
-							slackChat.addReceivedMsgtoDOM(parsedData.text);
 							$.getJSON('https://slack.com/api/channels.info',
 							{
 								token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
@@ -31,46 +30,6 @@ $($(document).ready( function() {
 						}
 					};
 				});
-		},
-		addMsgtoSlack: function(msgContent, channelName) {
-			var msg = {//TODO sending messages to Slack now works using websockets.  add sending using websockets in this function
-				type: "message",
-				channel: channelName,
-				text: msgContent,
-				user: "GuestBot",
-				ts: "1355517523.000005"
-			};
-			console.log('msg should be added to slack using websocket');
-
-			function sendtoSlack() {
-				slackChat.connection.send(JSON.stringify(msg));
-			}
-			if (slackChat.connectionDeferred.status()==="resolved") sendtoSlack();
-			else slackChat.connectionDeferred.done(sendtoSlack);
-		},
-		sendSMS: function(smsText, toPhoneNumber) { //new Slack message -> send out SMS texts using Twilio
-			$.ajax({
-				url: 'https://api.twilio.com/2010-04-01/Accounts/AC4e170477bd4abe4c97d8818f156ea4fb/Messages',
-				type: 'POST',
-				dataType: 'json',
-				headers: {
-					authorization: "Basic QUM0ZTE3MDQ3N2JkNGFiZTRjOTdkODgxOGYxNTZlYTRmYjo3OWVkZTMzM2NiZTc0NTI0MmM1OGVlZDk4Zjg1MGEwMA=="
-				},
-				data: {
-					From: '+13105041517',
-					To: toPhoneNumber,
-					Body: smsText,
-				}
-			})
-			.done(function() {
-				console.log("twilio success");
-			})
-			.fail(function() {
-				console.log("twilio error");
-			})
-			.always(function() {
-				console.log("twilio complete");
-			});
 		},
 		checkLatestSMS: function() {
 			//look through twilio REST API sms history.  somehow see if there is a new one (maybe using length of returned array??)
@@ -97,25 +56,21 @@ $($(document).ready( function() {
 
 				var channelString = jsonTwilio.messages[0].from;
 				channelString = channelString.substr(1);
-				channelString = JSON.stringify(channelString);
+				//channelString = JSON.stringify(channelString);
 				//console.log(channelString);
 
 				//createChannel(channelString);
 				createChannel(channelString, compareThenSendtoSlack);
 
-				function createChannel(channelID, callback) {
+				function createChannel(channelString, callback) {
 					$.ajax({
 						url: 'https://slack.com/api/channels.create',
-						type: 'GET',
+						type: 'POST',
 						dataType: 'json',
-						data: {token: "xoxp-2315976778-2315977822-10394561350-ca4652", name: channelID}
+						data: {token: "xoxp-2315976778-2315977822-10394561350-ca4652", name: channelString}
 					})
 					.done(function(json) {
 						console.log("createChannel success");
-						console.log(json.ok);
-						if (!json.ok) {
-
-						}
 					})
 					.fail(function() {
 						console.log("createChannel fail");
@@ -125,24 +80,39 @@ $($(document).ready( function() {
 					})
 					.always(function() {
 						console.log("createChannel complete");
+						//use channelString to find the channelID of the correct slack channel
+						$.getJSON('https://slack.com/api/channels.list',
+							{token: 'xoxp-2315976778-2315977822-10394561350-ca4652'},
+							function(json, textStatus) {
+								for (var key in json.channels) {
+									if (json.channels[key].name === channelString) {
+										channelID = json.channels[key].id;
+										console.log("yahtzee!!");
+										callback(channelID, channelString);
+										return;
+									}
+								}
+						});
+
 					});
 				}
 
-				function compareThenSendtoSlack(channelString) {
+				function compareThenSendtoSlack(channelID, userName) {
 					//get list of channel messages to compare with twilio records and send message to slack if theres newer ones in twilio
 					$.getJSON('https://slack.com/api/channels.history',
 					{
 						token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
-						channel: channelString,
+						channel: channelID,
 					}, function(jsonSlack, textStatus) {
-						//console.log(jsonSlack);
+						console.log(jsonSlack);
 						//only pull slack messages from this user (not from the hotel).  and only pull twilio messages sent to the phone number of this channel
+						//TODO need to check for all new messages, not just the latest 1 new message
 						if ((jsonSlack.messages[0] !== undefined) && ((Math.floor((new Date(jsonTwilio.messages[0].date_created).valueOf())/1000)) > (jsonSlack.messages[0].ts).valueOf())) {
 							console.log("twilioTimeStamp > slackTimeStamp");
-							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelString);
+							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelID, userName );
 						}
 						else if (jsonSlack.messages[0] === undefined) {
-							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelString);
+							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelID, userName);
 						}
 					});
 				}
@@ -150,6 +120,45 @@ $($(document).ready( function() {
 			});
 
 
+		},
+		addMsgtoSlack: function(msgContent, channelID, userName) {
+			var msg = {
+				type: "message",
+				channel: channelID,
+				text: msgContent,
+				ts: new Date().valueOf()
+			};
+			console.log('msg should be added to slack using websocket');
+
+			function sendtoSlack() {
+				slackChat.connection.send(JSON.stringify(msg));
+			}
+			if (slackChat.connectionDeferred.state()==="resolved") sendtoSlack();
+			else slackChat.connectionDeferred.done(sendtoSlack);
+		},
+		sendSMS: function(smsText, toPhoneNumber) { //new Slack message -> send out SMS texts using Twilio
+			$.ajax({
+				url: 'https://api.twilio.com/2010-04-01/Accounts/AC4e170477bd4abe4c97d8818f156ea4fb/Messages',
+				type: 'POST',
+				dataType: 'json',
+				headers: {
+					authorization: "Basic QUM0ZTE3MDQ3N2JkNGFiZTRjOTdkODgxOGYxNTZlYTRmYjo3OWVkZTMzM2NiZTc0NTI0MmM1OGVlZDk4Zjg1MGEwMA=="
+				},
+				data: {
+					From: '+13105041517',
+					To: toPhoneNumber,
+					Body: smsText,
+				}
+			})
+			.done(function() {
+				console.log("twilio success");
+			})
+			.fail(function() {
+				console.log("twilio error");
+			})
+			.always(function() {
+				console.log("twilio complete");
+			});
 		}
 	};
 
