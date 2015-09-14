826cd77f822829abb24f04ef3ecac8a0e851eac7
David Gonen <david@curiousminds.com>
2015-09-14 09:46:12 +0300

    some code cleanup/refactoring.  added a loop to checkLatestSMS() so that all potential new SMS from twilio are looked at to see if they have been added to the slack channel yet

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $($(document).ready( function() {
-
 	var slackChat = {
-		openSocket: function() {//this is used to detect messages created in Slack and then send them via Twilio SMS
+		openSocket: function() { //this is used to detect messages created in Slack and then send them via Twilio SMS
 			slackChat.connectionDeferred = new $.Deferred();
 			$.getJSON('https://slack.com/api/rtm.start',
 			 {token: 'xoxp-2315976778-2315977822-10394561350-ca4652'
@@ -12,11 +11,9 @@ $($(document).ready( function() {
 						slackChat.connectionDeferred.resolve();
 					};
 					connection.onerror = function (error) {
-				  //	console.log('Error Logged: ' + error); //log errors
 						slackChat.connectionDeferred.reject();
 					};
 					connection.onmessage = function (e) { //message event slack->app
-					 //console.log(e.data);
 						var parsedData = JSON.parse(e.data);
 						if ((parsedData.text !== undefined)&&(parsedData.subtype !== "bot_message")&&(parsedData.reply_to === undefined)&&(parsedData.subtype !== "channel_join")) {
 							$.getJSON('https://slack.com/api/channels.info',
@@ -31,8 +28,7 @@ $($(document).ready( function() {
 					};
 				});
 		},
-		checkLatestSMS: function() {
-			//look through twilio REST API sms history.  somehow see if there is a new one (maybe using length of returned array??)
+		checkLatestSMS: function() { //look through twilio REST API sms history.  somehow see if there is a new one (maybe using length of returned array??)
 			$.ajax({
 				url: 'https://api.twilio.com/2010-04-01/Accounts/AC4e170477bd4abe4c97d8818f156ea4fb/Messages.json',
 				type: 'GET',
@@ -45,24 +41,21 @@ $($(document).ready( function() {
 				}
 			})
 			.done(function(jsonTwilio) {
-
 			})
 			.fail(function() {
 				console.log("twilio error");
 			})
-			.always(function(jsonTwilio) {
+			.always(function(jsonTwilio) {  //pull latest twilio sms timestamp.  pull latest slack message timestamp.  if twilio sms timestamp > slack message timestamp push twilio sms to slack as message
 				console.log("twilio complete");
-				//pull latest twilio sms timestamp.  pull latest slack message timestamp.  if twilio sms timestamp > slack message timestamp push twilio sms to slack as message
 
-				var channelString = jsonTwilio.messages[0].from;
-				channelString = channelString.substr(1);
-				//channelString = JSON.stringify(channelString);
-				//console.log(channelString);
+				for (var z=0; z < jsonTwilio.messages.length; z++ ) {
+					var channelString = jsonTwilio.messages[z].from; //TODO-RYAN: go over how this loop works using breakpoints, not sure i understand what its actually doing.
+					channelString = channelString.substr(1);
+					//channelString = JSON.stringify(channelString);
+					createChannel(channelString, compareThenSendtoSlack, z);
+				}
 
-				//createChannel(channelString);
-				createChannel(channelString, compareThenSendtoSlack);
-
-				function createChannel(channelString, callback) {
+				function createChannel(channelString, callback, msgNumber) {
 					$.ajax({
 						url: 'https://slack.com/api/channels.create',
 						type: 'POST',
@@ -88,7 +81,7 @@ $($(document).ready( function() {
 									if (json.channels[key].name === channelString) {
 										channelID = json.channels[key].id;
 										console.log("yahtzee!!");
-										callback(channelID, channelString);
+										callback(channelID, channelString, msgNumber);
 										return;
 									}
 								}
@@ -97,29 +90,22 @@ $($(document).ready( function() {
 					});
 				}
 
-				function compareThenSendtoSlack(channelID, userName) {
-					//get list of channel messages to compare with twilio records and send message to slack if theres newer ones in twilio
+				function compareThenSendtoSlack(channelID, userName, msgNumber) { //get list of channel messages to compare with twilio records and send message to slack if theres newer ones in twilio
 					$.getJSON('https://slack.com/api/channels.history',
 					{
 						token: 'xoxp-2315976778-2315977822-10394561350-ca4652',
 						channel: channelID,
-					}, function(jsonSlack, textStatus) {
-						console.log(jsonSlack);
-						//only pull slack messages from this user (not from the hotel).  and only pull twilio messages sent to the phone number of this channel
-						//TODO need to check for all new messages, not just the latest 1 new message
-						if ((jsonSlack.messages[0] !== undefined) && ((Math.floor((new Date(jsonTwilio.messages[0].date_created).valueOf())/1000)) > (jsonSlack.messages[0].ts).valueOf())) {
+					}, function(jsonSlack, textStatus) { //only pull slack messages from this user (not from the hotel).  and only pull twilio messages sent to the phone number of this channel
+						if ((jsonSlack.messages[msgNumber] !== undefined) && ((Math.floor((new Date(jsonTwilio.messages[msgNumber].date_created).valueOf())/1000)) > (jsonSlack.messages[msgNumber].ts).valueOf())) {
 							console.log("twilioTimeStamp > slackTimeStamp");
-							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelID, userName );
+							slackChat.addMsgtoSlack(jsonTwilio.messages[msgNumber].body, channelID, userName );
 						}
-						else if (jsonSlack.messages[0] === undefined) {
-							slackChat.addMsgtoSlack(jsonTwilio.messages[0].body, channelID, userName);
+						else if (jsonSlack.messages[msgNumber] === undefined) {
+							slackChat.addMsgtoSlack(jsonTwilio.messages[msgNumber].body, channelID, userName);
 						}
 					});
 				}
-
 			});
-
-
 		},
 		addMsgtoSlack: function(msgContent, channelID, userName) {
 			var msg = {
@@ -128,8 +114,6 @@ $($(document).ready( function() {
 				text: msgContent,
 				ts: new Date().valueOf()
 			};
-			console.log('msg should be added to slack using websocket');
-
 			function sendtoSlack() {
 				slackChat.connection.send(JSON.stringify(msg));
 			}
